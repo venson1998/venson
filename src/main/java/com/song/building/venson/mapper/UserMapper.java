cbe732f12ac866c02dc97c6cee50d1ad63ca7292
venson1998 <1366237319@qq.com>
2020-03-20 22:00:50 +0800

    login over

--- src/main/java/com/song/building/venson/mapper/UserMapper.java
+++ src/main/java/com/song/building/venson/mapper/UserMapper.java
@@ -3,6 +3,8 @@ package com.song.building.venson.mapper;
 import com.song.building.venson.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper {
@@ -10,4 +12,6 @@ public interface UserMapper {
     @Insert("insert into user (name,account_id,token,gmt_create,gmt_modified,avatar_url) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified},#{avatarUrl})")
     public void insert(User user);
 
+    @Select("select * from user where token = #{token}")
+    User findByToken(@Param("token") String token);
 }
