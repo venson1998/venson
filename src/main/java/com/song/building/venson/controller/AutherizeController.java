80ac9b8e153b1094d4a6b2816df5ecfdee50596a
venson1998 <1366237319@qq.com>
2020-03-20 17:37:35 +0800

    login 2

---
+++ src/main/java/com/song/building/venson/controller/AutherizeController.java
@@ -0,0 +1,47 @@
+package com.song.building.venson.controller;
+
+import com.song.building.venson.dto.AccessTokenDTO;
+import com.song.building.venson.dto.GithubUser;
+import com.song.building.venson.provider.GithubProvider;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AutherizeController {
+
+    @Autowired
+    private GithubProvider githubProvider;
+
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.redirect_uri}")
+    private String redirectUrl;
+
+
+    //accessToken携带code获取github的accessToken
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name = "code") String code,
+                           @RequestParam(name = "state") String state){
+
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_uri(redirectUrl);
+        accessTokenDTO.setState(state);
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user.getName());
+
+
+        return "index";
+    }
+
+}
---
+++ src/main/java/com/song/building/venson/dto/GithubUser.java
@@ -0,0 +1,10 @@
+package com.song.building.venson.dto;
+
+import lombok.Data;
+
+@Data
+public class GithubUser {
+    private String name;
+    private Long id;
+    private String bio;
+}
